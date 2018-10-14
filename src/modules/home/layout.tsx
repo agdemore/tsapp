f2dd59f56946d4e0b0a4fc3f73f221b2acb4a1a2
agdemore <agdemore@gmail.com>
2018-10-14 11:00:36 +0300

    Add counter example to home

--- src/modules/home/layout.tsx
+++ src/modules/home/layout.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
+import { Provider } from 'react-redux';
+
+import { Counter } from '../../components/Counter/hoc';
+
+import { store } from '../common-store';
 
 export class HomeLayout extends React.Component<any> {
   render() {
     return (
-      <div>
-        Hello Component
-      </div>
+      <Provider store={store}>
+        <div>
+          <h3>Hello Component</h3>
+          <Counter />
+        </div>
+      </Provider>
     );
   }
 }
